ed5a8c8ae45ce39b957d8915a82f05db5810df52
이름 <>
2020-05-05 18:48:30 +0900

    add 서버상태2

--- index.js
+++ index.js
@@ -52,7 +52,19 @@ client.on('message', (message) => {
     embed.addField('Discord.js',   `v${Discord.version}`, true);
     embed.addField('Node',         `${process.version}`, true);
 
-    message.channel.send(embed)
+    let arr = client.guilds.array();
+    let list = '';
+    list = `\`\`\`css\n`;
+    
+    for(let i=0;i<arr.length;i++) {
+      // list += `${arr[i].name} - ${arr[i].id}\n`
+      list += `${arr[i].name}\n`
+    }
+    list += `\`\`\`\n`
+    embed.addField('list:',        `${list}`);
+
+    embed.setTimestamp()
+    message.channel.send(embed);
   }
 
   if(message.content == ';;도움') {
